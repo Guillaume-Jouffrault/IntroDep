9db57c342f1fc6fa27b341d3064d3e9a84c688b3
Guillaume-Jouffrault <guillaume.jouffrault@depinfonancy.net>
2021-06-09 09:55:32 +0200

    exos

---
+++ TD-C/module.h
@@ -0,0 +1,10 @@
+#pragma once
+int addOne(int n);
+int fibonacci(int n);
+void stock_nb_pairs(int t[],int n);
+void affichage(int t[], int n);
+void somme_tab(int u[],int v[],int s[],int n);
+int mylen(const char s[]);
+int mylen2(char* s);
+char* char_dynamique(char s[]);
+int nb_de_e(char* s);
